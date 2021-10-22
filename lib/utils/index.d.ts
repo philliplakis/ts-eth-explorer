340e0c7d599657daefd9a1807cc4ddbb6e7ad531
stratroulettehub <p.lakis@icloud.com>
2021-10-22 13:51:07 +1100

    remove build

--- lib/utils/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { AbiItem } from "web3-utils";
-export declare const ABI: Array<AbiItem>;
-export declare const log: {
-    write: (msg: string) => void;
-    success: (msg: string) => void;
-    warn: (msg: string) => void;
-    alert: (msg: string) => void;
-    error: (msg: string) => void;
-};
